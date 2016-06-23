7b0518d4af382aac854fbd2c2f433ff2a4c28b4c
Louis Magnotti <magnottila@gmail.com>
2016-06-23 16:20:48 -0400

    Completed final live exercise

--- primes.rb
+++ primes.rb
@@ -3,3 +3,23 @@
 
 # find_primes(3, 10)
 # => 3, 5, 7
+
+def is_prime?(number)
+  (2..(number - 1)).each do |divisor|
+    return false if number % divisor == 0
+  end
+
+  true
+end
+
+def find_primes(start, stop)
+  (start..stop).select do |number|
+    # check if number is prime
+    # prime is divisible only by 1 and itself
+    is_prime?(number)
+  end
+end
+
+p find_primes(3, 10)
+p find_primes(5, 20)
+p find_primes(1, 2)
